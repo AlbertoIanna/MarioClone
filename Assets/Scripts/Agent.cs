d87667a378fd6fe3e5272236db3dea2f92090c0f
MI_C_01\Ianna A <ianna.alberto00@gmail.com>
2018-01-16 12:27:09 +0100

    Clonando progetto MarioClone

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+    /// <summary>
+    /// Nome dell'agent.
+    /// </summary>
+    public string Name;
+    public int Life;
+    protected int InitialLife;
+    public bool IsAlive = true;
+    /// <summary>
+    /// Velocità di movimento (maggiore è il valore, maggiore sarà la velocità di spostamento)
+    /// </summary>
+    public float MovementSpeed;
+    /// <summary>
+    /// Forza con coi avviene la spinta verso l'alto.
+    /// </summary>
+    public float JumpForce = 10;
+    /// <summary>
+    /// Identifica se il personaggio sta attualmenmte saltando (se true sta saltando).
+    /// </summary>
+    public bool IsJumping = false;
+    public Rigidbody rigidbody = null;
+    public float RespawnDelay;
+    protected Vector3 respawnposition;
+
+    private void Awake()
+    {
+        respawnposition = transform.position;
+        InitialLife = Life;
+    }
+    /// <summary>
+    /// Toglie una unità di salute a questo oggetto.
+    /// </summary>
+    public  void Damage(int DamageTaken)
+    {
+        Debug.Log("Ho subito un danno " + Name);
+        Life = Life - DamageTaken;
+        if (Life <= 0 )
+        {
+            IsAlive = false;
+            if (gameObject.GetComponent<MeshRenderer>() != null)
+            {
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }
+    public virtual void Kill()
+    {
+        Damage(Life);
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Agent {
+
+    public Vector3 ArrivePoint;
+    public bool GotoRight ;
+    private void Start()
+    {
+        transform.position = respawnposition;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag== "Player"){
+            other.GetComponent<BasePlayer>().Damage(2);
+        }
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    public override void Kill()
+    {
+        base.Kill();
+        Destroy(this.gameObject);
+    }
+  
+    public void Movement() {
+        if (GotoRight==true)
+        {
+                transform.Translate(Vector3.right * Time.deltaTime);
+            if(transform.position.x >= ArrivePoint.x)
+                GotoRight= false;
+        }
+        else
+        { 
+                transform.Translate(Vector3.left * Time.deltaTime);
+            if( transform.position.x <= respawnposition.x)
+                GotoRight = true;
+        }
+    }
+}
+
